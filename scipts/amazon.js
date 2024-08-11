49334731f767c53038c75e53945c87d7b9ba1342
invincibolt <gaming1royal2@gmail.com>
2024-08-11 12:18:27 +0530

    made cartQuantity interactive

--- scipts/amazon.js
+++ scipts/amazon.js
@@ -71,7 +71,17 @@ document.querySelectorAll(".js-addtocart").forEach((button)=>{
           quantity: 1
         })
       }
-    
+      let cartQuantity=0;
+      cart.forEach((item)=> {
+        cartQuantity+=item.quantity;
+      })
+
+    document.querySelector(".js-cart-quantity").innerHTML=cartQuantity;
+
+    console.log(cartQuantity);
     console.log(cart);
   })
+  
+
+
 })
