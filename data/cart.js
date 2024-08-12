0d234d4c35e368ed062dba8cec7bbfb6da506ccd
invincibolt <gaming1royal2@gmail.com>
2024-08-13 02:09:28 +0530

    save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,13 @@
 export let cart = JSON.parse(localStorage.getItem('cart')) ||
-[];
+[{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  itemQuantity: 2,
+  deliveryOptionId: '1'
+},{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  itemQuantity: 1,
+  deliveryOptionId: '2'
+}];
 
 function saveToStorage(){
   localStorage.setItem('cart',JSON.stringify(cart))
@@ -22,7 +30,8 @@ export function addToCart(productId){
     else{
       cart.push({
         productId,
-        itemQuantity
+        itemQuantity,
+        deliveryOptionId: '1'
       })
     }
     saveToStorage();
